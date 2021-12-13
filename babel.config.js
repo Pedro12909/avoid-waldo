abcb3af04dd36a4b231bc5ce21217ea9a5fd6553
Pedro12909 <pedro12909@hotmail.com>
2021-12-13 23:38:44 +0000

    fixes parcel warnings about babel

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
     plugins: [
         '@babel/plugin-transform-modules-commonjs',
         '@babel/plugin-proposal-class-properties',
